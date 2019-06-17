20d4a83c8dedc6a9e59987294f7347a94a2c3a84
jykhli@gmail.com <jykhli@gmail.com>
2019-06-17 02:02:09 +0200

    Fierbase setup

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -2,11 +2,40 @@
   <div>
     <h1>Products</h1>
     <p>This view display a list of products. In this view we can also do CRUD operations on products.</p>
+    <hr>
+    <ul>
+      <li :key="product.id" v-for="product in products">
+        {{product.name}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {};
+import db from '../firebase/firebaseInit'
+export default {
+  data () {
+        return {
+            products: []
+        }
+    },
+    created() {
+        db.collection('products').orderBy('name').get().then
+        (querySnapshot => {
+            querySnapshot.forEach(doc => {
+                const data = {
+                    'id' : doc.id,
+                    'name' : doc.data().name,
+                    'brand' : doc.data().brand,
+                    'category' : doc.data().category,
+                    'company' : doc.data().company,
+                    'description' : doc.data().description,
+                }
+                this.products.push(data);
+            })
+        })
+    }
+};
 </script>
 
 <style>
